de51ac9c4b179fee9254f53b4364e8da977f6501
itaquito <ichilaquiles@gmail.com>
2024-03-27 11:31:31 -0600

    feat: Adds Github link in NavBar

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,14 +1,21 @@
 import IcBaselineQrCode from '../Icon/IcBaselineQrCode';
+import MdiGithub from '../Icon/MdiGithub';
 
 function NavBar() {
   return (
-    <nav className='flex w-full items-center bg-white px-12 py-4 shadow'>
-      <IcBaselineQrCode className='mr-2 h-[50px] w-[50px]' />
-      <div>
-        <h1 className='text-3xl font-bold text-primary'>QR Code Generator</h1>
-        Free, simple, no ads, no trackers, open source and everything runs on
-        your browser.
+    <nav className='flex w-full items-center justify-between bg-white px-12 py-4 shadow'>
+      <div className='flex items-center'>
+        <IcBaselineQrCode className='mr-2 h-[50px] w-[50px]' />
+        <div>
+          <h1 className='text-3xl font-bold text-primary'>QR Code Generator</h1>
+          Free, simple, no ads, no trackers, open source and everything runs on
+          your browser.
+        </div>
       </div>
+
+      <a href='https://github.com/itaquito/qr-generator' target='_blank'>
+        <MdiGithub className='h-[25px] w-[25px] transition-all hover:text-primary' />
+      </a>
     </nav>
   );
 }
